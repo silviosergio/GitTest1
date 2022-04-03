c7767ca4cc1f234567deb8b851a0351ec09d2b8a
Silvio <silvio.sergio@libero.it>
2022-04-04 00:42:19 +0200

    Added Feature1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,8 @@ namespace ConsoleApp1
         {
             //VS undo
             Console.WriteLine("Hello World!!!");
+            //code to call feature 1
+            Feature1 feature1 = new Feature1();
         }
     }
 }
