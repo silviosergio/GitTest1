cf73e982bc022718e0f021a33ee7e4cc85c5a297
silviosergio <80702884+silviosergio@users.noreply.github.com>
2022-04-04 00:23:57 +0200

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             //code was edited in github
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!!");
         }
     }
 }
